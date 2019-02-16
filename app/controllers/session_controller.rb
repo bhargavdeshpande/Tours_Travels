49fd80ed3b5cd63e2ddb92f2ed312a9682eb32ec
nparnaj <nparanj@ncsu.edu>
2019-02-15 20:09:39 -0500

    Functionality to login and establish session

---
+++ app/controllers/session_controller.rb
@@ -0,0 +1,19 @@
+class SessionController < ApplicationController
+  def new
+  end
+
+  def create
+    login = Login.find_by(name: params[:name])
+    if login and login.authenticate(params[:password])
+      sessions[:user_id] = login.id
+      redirect_to login_url, alert "Logged in successfully"
+    else
+      redirect_to logi_url, alert "Unsuccessful login!"
+    end
+  end
+
+  def destroy
+    session[:user_id] = Nil
+    redirect_to logins_url
+  end
+end
