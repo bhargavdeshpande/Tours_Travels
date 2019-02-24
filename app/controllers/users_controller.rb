3c4c3769087affc7f152e96fbdc2dfc125586c05
Nitin Nataraj Kuncham <nitin.kuncham@gmail.com>
2019-02-24 17:20:55 -0500

    changed links

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -67,7 +67,11 @@ class UsersController < ApplicationController
       		format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
       		format.json { head :no_content }
     	end
-    end
+    else
+    	respond_to do |format|
+      		format.html { redirect_to users_url, notice: 'Admin account cannot be destroyed.' }
+	end
+    end	
   end
 
   private
